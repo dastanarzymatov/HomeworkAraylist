a2bee76c2b071020720e31fab039fc229335d3e5
dasta <arzymatodastan@gmail.com>
2022-06-02 03:31:24 +0600

    work

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        ArrayList<Object> arrayList=new ArrayList<>();
+        ArrayList<Object> arrayList2=new ArrayList<>();
+        ArrayList<Object> arrayList3=new ArrayList<>();
+
+        Random ran=new Random();
+        for (int i = 0; i <=50 ; i++) {
+            int a= ran.nextInt(1,101);
+           arrayList.add(a);
+            if (a % 2 == 0) {
+               arrayList2.add(a);
+            } else {
+                arrayList3.add(a);
+            }
+        }
+        System.out.print("chykkan sandar "+arrayList+"\n");
+        System.out.print("jup sandar "+arrayList2+"\n");
+        System.out.print(" tak sandar "+arrayList3);
+
     }
 }
